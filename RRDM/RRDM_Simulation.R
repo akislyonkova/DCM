fc62ac3c356c6e02623893f2f415bb07c3c19a15
Alexandra <kislenkova.oskemen@gmail.com>
2023-11-03 10:45:25 -0700

    Fixed the bugs, rearranged the order of the lines. This code produces 1 simulated RRDM dataset at a time.

--- RRDM/RRDM_Simulation.R
+++ RRDM/RRDM_Simulation.R
@@ -1,6 +1,19 @@
-# Simulate dataset
+# Simulate a dataset
 
-set.seed(666)
+# a document with parameters for an RRDM 
+#library("rstan")
+#RRDM<-summary(estimated_rrdm)$summary
+# library(rio)
+# steps <- import("step.xlsx")
+# item <- import("item.xlsx")
+# 
+# step <- (round(steps,4)) # rounds the step parameters to the 4th decimal point 
+# item<-(round(item,4))
+# t <- matrix(NA,40,5) # empty T matrix 40 questions and 4 profiles (item by profile matrix)
+# r <- matrix(NA,901,40) # empty response matrix, 40 questions, 901 people 
+# responses <- list() # creates an empty list for holding responses 
+
+#set.seed(666)
 Q=matrix(c(rep(c(1,0,0,0),10),rep(c(0,1,0,0),10),rep(c(0,0,1,0),10),rep(c(0,0,0,1),10)),40,4, byrow=T) # 40 questions and 4 attributes 
 colnames(Q) <- c("A1","A2", "A3", "A4")
 n_attr<-dim(Q)[2]
@@ -13,18 +26,6 @@ N <- 901 # number of people
 ind <- sample(x=1:AP , size=N, replace = TRUE , prob=alpha.prob)
 alpha <- alpha.patt[ind,] # simulated pattern ("truth") for all attributes for each person 
 
-# a document with parameters for an RRDM 
-#library("rstan")
-#RRDM<-summary(estimated_rrdm)$summary
-library(rio)
-steps <- import("step.xlsx")
-item <- import("item.xlsx")
-
-steps <- (round(steps,4)) # rounds the step parameters to the 4th decimal point 
-item<-(round(item,4))
-t <- matrix(NA,40,5) # empty T matrix 40 questions and 4 profiles (item by profile matrix)
-r <- matrix(NA,901,40) # empty response matrix, 40 questions, 901 people 
-responses <- list() # creates an empty list for holding responses 
 
 # this is the main part 
 # the loops generate responses for each person on each question based on the alpha patterns 
@@ -41,10 +42,10 @@ for(k in 1:nrow(alpha)) { # k - person
     r[k,i]=which(ppp == 1, arr.ind=TRUE)[1] #which() function returns the position/index of the value
   }
   for(i in 11:20) { # questions 11 through 20 (2nd attribute) 
-    t1=exp(1(item[i,1]+item[i,2]*alpha[k,2]) + step[i,1])
-    t2=exp(2(item[i,1]+item[i,2]*alpha[k,2]) + step[i,1] + step[i,2])
-    t3=exp(3(item[i,1]+item[i,2]*alpha[k,2]) + step[i,1] + step[i,2] + step[i,3])
-    t4=exp(4(item[i,1]+item[i,2]*alpha[k,2]) + step[i,1] + step[i,2] + step[i,3] + step[i,4])
+    t1=exp(1*(item[i,1]+item[i,2]*alpha[k,2]) + step[i,1])
+    t2=exp(2*(item[i,1]+item[i,2]*alpha[k,2]) + step[i,1] + step[i,2])
+    t3=exp(3*(item[i,1]+item[i,2]*alpha[k,2]) + step[i,1] + step[i,2] + step[i,3])
+    t4=exp(4*(item[i,1]+item[i,2]*alpha[k,2]) + step[i,1] + step[i,2] + step[i,3] + step[i,4])
     sum=1+t1+t2+t3+t4
     t[i,]=c(1,t1/sum,t2/sum,t3/sum, t4/sum) 
     ppp=rmultinom(n=1, size=1, prob=t[i,]) # n- number of random vectors, prob - probabilities sum=1
@@ -71,7 +72,11 @@ for(k in 1:nrow(alpha)) { # k - person
     r[k,i]=which(ppp == 1, arr.ind=TRUE)[1]
   }
 }
-responses[[length(responses)+1]]=as.data.frame(r)
 
-save(responses, file = "simresponses.rda")
+export(r,'dataset10.xlsx')
+
+
+# responses[[length(responses)+1]]=as.data.frame(r)
+# save(responses, file = "simresponses.rda")
+
 
