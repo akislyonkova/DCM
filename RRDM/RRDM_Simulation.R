637ff20b18de66560c5cf6b50b6ad8d0f7522433
Alexandra <kislenkova.oskemen@gmail.com>
2023-11-03 11:12:03 -0700

    Cleaned up the code, deleted the extra lines that are not being used, e.g. summary function.

--- RRDM/RRDM_Simulation.R
+++ RRDM/RRDM_Simulation.R
@@ -1,37 +1,31 @@
 # Simulate a dataset
 
-# a document with parameters for an RRDM 
-#library("rstan")
-#RRDM<-summary(estimated_rrdm)$summary
-# library(rio)
-# steps <- import("step.xlsx")
-# item <- import("item.xlsx")
-# 
-# step <- (round(steps,4)) # rounds the step parameters to the 4th decimal point 
-# item<-(round(item,4))
-# t <- matrix(NA,40,5) # empty T matrix 40 questions and 4 profiles (item by profile matrix)
-# r <- matrix(NA,901,40) # empty response matrix, 40 questions, 901 people 
+library(rio)
+steps <- import("step.xlsx")
+item <- import("item.xlsx")
+
+step <- (round(steps,4)) # rounds the step parameters to the 4th decimal point
+item<-(round(item,4))
+t <- matrix(NA,40,5) # empty T matrix 40 questions and 4 profiles (item by profile matrix)
+r <- matrix(NA,901,40) # empty response matrix, 40 questions, 901 people
 # responses <- list() # creates an empty list for holding responses 
 
-#set.seed(666)
-Q=matrix(c(rep(c(1,0,0,0),10),rep(c(0,1,0,0),10),rep(c(0,0,1,0),10),rep(c(0,0,0,1),10)),40,4, byrow=T) # 40 questions and 4 attributes 
+set.seed(666)
+Q=matrix(c(rep(c(1,0,0,0),10),rep(c(0,1,0,0),10),rep(c(0,0,1,0),10),rep(c(0,0,0,1),10)),40,4, byrow=T) # 40 questions and 4 attributes
 colnames(Q) <- c("A1","A2", "A3", "A4")
 n_attr<-dim(Q)[2]
-alpha.patt<-(expand.grid(replicate(n_attr, 0:1, simplify = FALSE))) # profile set 
-AP <- nrow(alpha.patt) # extracts the number of profiles 
+alpha.patt<-(expand.grid(replicate(n_attr, 0:1, simplify = FALSE))) # profile set
+AP <- nrow(alpha.patt) # extracts the number of profiles
 x <- runif(16, min = 0, max = 1) # generate 16 random values between 1 and 0
-x1 <- x/sum(x) # normalize this vector so that the sum is equal to 1 
+x1 <- x/sum(x) # normalize this vector so that the sum is equal to 1
 alpha.prob <- x1 # random values between 0 and 1 that give 1 in sum
-N <- 901 # number of people 
+N <- 901 # number of people
 ind <- sample(x=1:AP , size=N, replace = TRUE , prob=alpha.prob)
-alpha <- alpha.patt[ind,] # simulated pattern ("truth") for all attributes for each person 
+alpha <- alpha.patt[ind,] # simulated pattern ("truth") for all attributes for each person
 
-
-# this is the main part 
 # the loops generate responses for each person on each question based on the alpha patterns 
-
-for(k in 1:nrow(alpha)) { # k - person 
-  for(i in 1:10) { # i - item, questions 1 through 10 (1st attribute) 
+for(k in 1:nrow(alpha)) {  # k - person 
+  for(i in 1:10) {        # i - item, questions 1 through 10 (1st attribute) 
     t1=exp(1*(item[i,1]+item[i,2]*alpha[k,1]) + step[i,1])
     t2=exp(2*(item[i,1]+item[i,2]*alpha[k,1]) + step[i,1] + step[i,2])
     t3=exp(3*(item[i,1]+item[i,2]*alpha[k,1]) + step[i,1] + step[i,2] + step[i,3])
@@ -72,6 +66,7 @@ for(k in 1:nrow(alpha)) { # k - person
     r[k,i]=which(ppp == 1, arr.ind=TRUE)[1]
   }
 }
+
 
 export(r,'dataset10.xlsx')
 
