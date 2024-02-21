78a53d56d850a0bd40dc855040ec39184e6b24ba
Alexandra Kislyonkova <kislenkova.oskemen@gmail.com>
2024-02-21 10:33:05 -0800

    Fixed a bug in data generation: added the expansion of step parameters.

--- RRDM/RRDM_Simulation.R
+++ RRDM/RRDM_Simulation.R
@@ -3,6 +3,7 @@
 library(rio)
 steps <- import("step.xlsx")
 item <- import("item.xlsx")
+steps <- steps[rep(seq_len(nrow(steps)), each = 10), ]
 
 step <- (round(steps,4)) # rounds the step parameters to the 4th decimal point
 item<-(round(item,4))
@@ -10,7 +11,7 @@ t <- matrix(NA,40,5) # empty T matrix 40 questions and 4 profiles (item by profi
 r <- matrix(NA,901,40) # empty response matrix, 40 questions, 901 people
 # responses <- list() # creates an empty list for holding responses 
 
-set.seed(666)
+set.seed(2024)
 Q=matrix(c(rep(c(1,0,0,0),10),rep(c(0,1,0,0),10),rep(c(0,0,1,0),10),rep(c(0,0,0,1),10)),40,4, byrow=T) # 40 questions and 4 attributes
 colnames(Q) <- c("A1","A2", "A3", "A4")
 n_attr<-dim(Q)[2]
@@ -31,55 +32,46 @@ for(k in 1:nrow(alpha)) {  # k - person
     t3=exp(3*(item[i,1]+item[i,2]*alpha[k,1]) + step[i,1] + step[i,2] + step[i,3])
     t4=exp(4*(item[i,1]+item[i,2]*alpha[k,1]) + step[i,1] + step[i,2] + step[i,3] + step[i,4])
     sum=1+t1+t2+t3+t4
-    t[i,]=c(1/sum, t1/sum,t2/sum,t3/sum, t4/sum) 
+    t[i,] = c(1/sum, t1/sum,t2/sum,t3/sum, t4/sum)
     ppp=rmultinom(n=1, size=1, prob=t[i,]) # n- number of random vectors, prob - probabilities sum=1
     r[k,i]=which(ppp == 1, arr.ind=TRUE)[1] #which() function returns the position/index of the value
   }
-  for(i in 11:20) { # questions 11 through 20 (2nd attribute) 
+  for(i in 11:20) { # questions 11 through 20 (2nd attribute)
     t1=exp(1*(item[i,1]+item[i,2]*alpha[k,2]) + step[i,1])
     t2=exp(2*(item[i,1]+item[i,2]*alpha[k,2]) + step[i,1] + step[i,2])
     t3=exp(3*(item[i,1]+item[i,2]*alpha[k,2]) + step[i,1] + step[i,2] + step[i,3])
     t4=exp(4*(item[i,1]+item[i,2]*alpha[k,2]) + step[i,1] + step[i,2] + step[i,3] + step[i,4])
     sum=1+t1+t2+t3+t4
-    t[i,]=c(1/sum,t1/sum,t2/sum,t3/sum, t4/sum) 
+    t[i,]=c(1/sum,t1/sum,t2/sum,t3/sum, t4/sum)
     ppp=rmultinom(n=1, size=1, prob=t[i,]) # n- number of random vectors, prob - probabilities sum=1
     r[k,i]=which(ppp == 1, arr.ind=TRUE)[1] #which() function returns the position/index of the value
   }
-  for(i in 21:30) { # questions 21 through 30 (3rd attribute) 
+  for(i in 21:30) { # questions 21 through 30 (3rd attribute)
     t1=exp(1*(item[i,1]+item[i,2]*alpha[k,3]) + step[i,1])
     t2=exp(2*(item[i,1]+item[i,2]*alpha[k,3]) + step[i,1] + step[i,2])
     t3=exp(3*(item[i,1]+item[i,2]*alpha[k,3]) + step[i,1] + step[i,2] + step[i,3])
     t4=exp(4*(item[i,1]+item[i,2]*alpha[k,3]) + step[i,1] + step[i,2] + step[i,3] + step[i,4])
     sum=1+t1+t2+t3+t4
-    t[i,]=c(1/sum,t1/sum,t2/sum,t3/sum, t4/sum) 
-    ppp=rmultinom(n=1, size=1, prob=t[i,]) 
+    t[i,]=c(1/sum,t1/sum,t2/sum,t3/sum, t4/sum)
+    ppp=rmultinom(n=1, size=1, prob=t[i,])
     r[k,i]=which(ppp == 1, arr.ind=TRUE)[1]
   }
-  for(i in 31:40) { # questions 31 through 40 (4th attribute) 
+  for(i in 31:40) { # questions 31 through 40 (4th attribute)
     t1=exp(1*(item[i,1]+item[i,2]*alpha[k,4]) + step[i,1])
     t2=exp(2*(item[i,1]+item[i,2]*alpha[k,4]) + step[i,1] + step[i,2])
     t3=exp(3*(item[i,1]+item[i,2]*alpha[k,4]) + step[i,1] + step[i,2] + step[i,3])
     t4=exp(4*(item[i,1]+item[i,2]*alpha[k,4]) + step[i,1] + step[i,2] + step[i,3] + step[i,4])
     sum=1+t1+t2+t3+t4
-    t[i,]=c(1/sum,t1/sum,t2/sum,t3/sum, t4/sum) 
-    ppp=rmultinom(n=1, size=1, prob=t[i,]) 
+    t[i,]=c(1/sum,t1/sum,t2/sum,t3/sum, t4/sum)
+    ppp=rmultinom(n=1, size=1, prob=t[i,])
     r[k,i]=which(ppp == 1, arr.ind=TRUE)[1]
   }
 }
 
 
-export(r,'dataset1.xlsx')
+
+export(r,'dataset20.xlsx')
 
 
 # responses[[length(responses)+1]]=as.data.frame(r)
 # save(responses, file = "simresponses.rda")
-i=1
-k=1
-t1=exp(1*(item[i,1]+item[i,2]*alpha[k,1]) + step[i,1])
-t2=exp(2*(item[i,1]+item[i,2]*alpha[k,1]) + step[i,1] + step[i,2])
-t3=exp(3*(item[i,1]+item[i,2]*alpha[k,1]) + step[i,1] + step[i,2] + step[i,3])
-t4=exp(4*(item[i,1]+item[i,2]*alpha[k,1]) + step[i,1] + step[i,2] + step[i,3] + step[i,4])
-sum=1+t1+t2+t3+t4
-t[i,]=c(1/sum, t1/sum,t2/sum,t3/sum, t4/sum) 
-ppp=rmultinom(n=1, size=1, prob=t[i,]) # n- number of random vectors, prob - probabilities sum=1
-r[k,i]=which(ppp == 1, arr.ind=TRUE)[1]
