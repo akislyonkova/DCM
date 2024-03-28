c02cc97387e729dd8cf249dac2e1ff33f7825af5
Alexandra Kislyonkova <kislenkova.oskemen@gmail.com>
2024-03-28 11:45:12 -0700

    Started working on uploading simulated datasets and saving them in a list.

--- Clustering analysis/Clustering_analyze.R
+++ Clustering analysis/Clustering_analyze.R
@@ -4,12 +4,29 @@ library(GPArotation)
 library(lavaan)
 library(cluster)
 library(factoextra)
+library(rio)
 
-disc <- read.csv("discdat.csv")
-describe(disc)
+sim1 <- import("dataset20.xlsx")
+describe(sim1)
+names <- c('AS1', 'AS2', 'AS3', 'AS4', 'AS5', 'AS6', 'AS7', 'AS8', 'AS9', 'AS10',
+           'SC1', 'SC2', 'SC3', 'SC4', 'SC5', 'SC6', 'SC7', 'SC8', 'SC9', 'SC10',
+           'AD1', 'AD2', 'AD3', 'AD4', 'AD5', 'AD6', 'AD7', 'AD8', 'AD9', 'AD10',
+           'DO1', 'DO2', 'DO3', 'DO4', 'DO5', 'DO6', 'DO7', 'DO8', 'DO9', 'DO10')
+colnames(sim1) <- names
 
-test <- disc[c(1:450),]
-train <- disc[c(451:901),]  
+# simr <- list(NA)
+# n_sim <- 2
+# 
+# for (i in 1:n_sim){
+#   sim_name <- sprintf("dataset%i.xlsx", i)
+#   sim_i <- import(sim_name)
+#   simr <- simr + sim_i
+# }
+
+
+
+test <- sim1[c(1:450),]
+train <- sim1[c(451:901),]  
 
 poly_cor <- polychoric(train)
 rho <- poly_cor$rho
@@ -19,7 +36,7 @@ set.seed(2024) # for reproducibility
 distance <- dist(rho)
 
 hclust <- hclust(distance, method = "ward.D")
-plot(hclust, main='Dendrogram for DISC data')
+plot(hclust, main='Dendrogram for sim1 data')
 member <- cutree(hclust,4)
 table(member)
 
