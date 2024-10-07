b9e10da08fc3f4df395d656731b2d2ec7ae38dde
Alexandra <kislenkova.oskemen@gmail.com>
2024-10-07 14:20:29 -0700

    Updated the starting code: removed numbers from the object names, cahnges hte names in the bias calculation part, dleeted redundant actions.

--- RRDM/RRDM.R
+++ RRDM/RRDM.R
@@ -14,7 +14,7 @@ rrdm_model<-stan_model("RRDM.stan")
 date <- Sys.Date()
 
 start.time <- Sys.time()
-rrdm4<-sampling(rrdm_model,  
+rrdm<-sampling(rrdm_model,  
                   data=list(Y=respMatrix, 
                   Ns=5, 
                   Np=nrow(respMatrix), 
@@ -25,18 +25,17 @@ rrdm4<-sampling(rrdm_model,
 end.time <- Sys.time()
 time.taken <- end.time - start.time
 
-save(rrdm4, file =  paste('rrdm', n,'_', date, sep=''))  
-RRDM_4<-as.data.frame(summary(rrdm4)$summary) 
-#write.table(RRDM_4,'RRDMsim4.txt') 
-write.table(RRDM_4, file =  paste('RRDMsim', n,'.txt',sep='')) 
+save(rrdm, file =  paste('rrdm', n,'_', date, sep=''))  
+RRDM<-as.data.frame(summary(rrdm)$summary) 
 
-converged <- sum(na.omit(RRDM_4$Rhat) > 1.1)
+write.table(RRDM, file =  paste('RRDMsim', n,'.txt',sep='')) 
+
+converged <- sum(na.omit(RRDM$Rhat) > 1.1)
 
 rows_start <- 9 
 rows_end <- 80  
-rrdm_param <- read.table("cell1_param.txt") 
-RRDM <- read.table("RRDMsim4.txt") 
-sim_param <- data.frame(RRDM[c(rows_start:rows_end),1]) 
-bias <- rrdm_param - sim_param
+true_param <- read.table("cell1_param.txt") 
+sim_param <- RRDM[c(rows_start:rows_end),1] 
+bias <- true_param - sim_param
 colnames(bias) <- 'raw'
 hist(bias$raw)
