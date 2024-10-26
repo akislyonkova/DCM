a356b2c96040ef8a20458a89203e0bd9fbface48
Alexandra Kislyonkova <kislenkova.oskemen@gmail.com>
2024-10-25 17:10:54 -0700

    Updated the stan code geration an stan files for RRDM and RSDM for 3 attributes (dark3 dataset). Renamed RSDM code generation file to allign with the rest of the models.

--- Code genration/RSDM Stan Code Generation.R
+++ Code genration/RSDM Stan Code Generation.R
@@ -1,7 +1,6 @@
 library(stringr)
-library(rstan)
 
-RRDM<-function(Qmatrix,scale.num,save.path=getwd(),save.name="RRDM"){
+RSDM<-function(Qmatrix,scale.num,save.path=getwd(),save.name="RSDM"){
   nstep=scale.num-1
   n_attr  <- ncol(Qmatrix)
   n_items <- nrow(Qmatrix)
@@ -20,26 +19,47 @@ RRDM<-function(Qmatrix,scale.num,save.path=getwd(),save.name="RRDM"){
   #li_1 - item main effect, list with nitems length 
   li_1 <- rep(c("NA"), n_items)
   for (i in 1:n_items){
-    li_1[i]<-paste('l',i,'M', sep='')
+    li_1[i]<-paste('l',i,'M', sep='') 
   }
   #ls_0 - step intercepts, matrix with nitems by nsteps dimensions
   ls_0 <- matrix("NA", n_items,nstep)
   #loop for filling the step intercepts 
   for (s in 1:nstep){
     ls_0[,s] <- paste("step",s, "_I", sep='')
-    ls_0[1:9, s] <- paste(ls_0[1:9, s], "D1", sep='') # 10 step parameters for dimension 1
+    ls_0[1:9, s] <- paste(ls_0[1:9, s], "D1", sep='')
     ls_0[10:18, s] <- paste(ls_0[10:18, s], "D2", sep='')
     ls_0[19:27, s] <- paste(ls_0[19:27, s], "D3", sep='')
     #ls_0[31:40, s] <- paste(ls_0[31:40, s], "D4", sep='')
   }
   #creating a cumulative matrix with a sum of "-" step intercepts 
   ls_0_cumul <- ls_0
-  ls_0_cumul[,2] <- paste(ls_0[,1],'+',ls_0[,2],sep='')
-  ls_0_cumul[,3] <- paste(ls_0[,1],'+',ls_0[,2],'+',ls_0[,3],sep='')
-  ls_0_cumul[,4] <- paste(ls_0[,1],'+',ls_0[,2],'+',ls_0[,3],'+',ls_0[,4],sep='')
+  ls_0_cumul[,2] <- paste(ls_0[,1],'-',ls_0[,2],sep='')
+  ls_0_cumul[,3] <- paste(ls_0[,1],'-',ls_0[,2],'-',ls_0[,3],sep='')
+  ls_0_cumul[,4] <- paste(ls_0[,1],'-',ls_0[,2],'-',ls_0[,3],'-',ls_0[,4],sep='')
+  
+  #ls_1 - step main effects , matrix with nitems by nsteps dimensions
+  ls_1 <- matrix("NA", n_items,nstep)
+  #loop for filling the step main effects 
+  for (s in 1:nstep){
+    ls_1[,s] <- paste("step",s, "_M", sep='')
+    ls_1[1:9, s] <- paste(ls_1[1:9, s], "D1", sep='')
+    ls_1[10:18, s] <- paste(ls_1[10:18, s], "D2", sep='')
+    ls_1[19:27, s] <- paste(ls_1[19:27, s], "D3", sep='')
+    #ls_1[31:40, s] <- paste(ls_1[31:40, s], "D4", sep='')
+  }
+  #creating a cumulative matrix with a sum of "+" step main effects  
+  ls_1_cumul <- ls_1
+  ls_1_cumul[,2] <- paste(ls_1[,1],'+',ls_1[,2],sep='')
+  ls_1_cumul[,3] <- paste(ls_1[,1],'+',ls_1[,2],'+',ls_1[,3],sep='')
+  ls_1_cumul[,4] <- paste(ls_1[,1],'+',ls_1[,2],'+',ls_1[,3],'+',ls_1[,4],sep='')
+  
   
   ##################################################################################################################################  
   ls_0_unique <-unique(ls_0)
+  ls_1_unique <-unique(ls_1)
+  
+  
+  
   
   Reparm<-array(rep(0,n_items*nclass*(scale.num)),dim = c(n_items,nclass,(scale.num))) # placeholder for the loop results 
   
@@ -48,10 +68,9 @@ RRDM<-function(Qmatrix,scale.num,save.path=getwd(),save.name="RRDM"){
     for(loopi in 1:n_items){
       for(loopc in 1:nclass){
         Reparm[loopi,loopc,1]<-paste('  PImat[',loopi,',',loopc,'][1]=0;\n',sep='')
-        Reparm[loopi,loopc,loops+1]<-paste('  PImat[',loopi,',',loopc,'][',loops+1,']=', loops, '*(',li_0[loopi],
-                                           '+', li_1[loopi], '*', PfbyI[loopi,loopc], 
-                                           ")+" ,ls_0_cumul[loopi,loops],
-                                           ';\n',sep='')
+        Reparm[loopi,loopc,loops+1]<-paste('  PImat[',loopi,',',loopc,'][',loops+1,']=',li_0[loopi],
+                                           '-',ls_0_cumul[loopi,loops], '+(', li_1[loopi], 
+                                           "+" ,ls_1_cumul[loopi,loops],')*', PfbyI[loopi,loopc], ';\n',sep='')
       }
       
     }
@@ -65,7 +84,8 @@ RRDM<-function(Qmatrix,scale.num,save.path=getwd(),save.name="RRDM"){
                      paste( li_0,'~normal(0,2)',';\n'),
                      paste( li_1,'~normal(0,2)', ';\n'),
                      paste(ls_0_unique,'~normal(0,2)', ';\n'),
-                     paste('   Vc~dirichlet(rep_vector(2.0, Nc));',sep='')))
+                     paste( ls_1_unique,'~normal(0,2)',';\n'),
+                     paste('Vc~dirichlet(rep_vector(2.0, Nc));',sep='')))
   
   #Likelihood Stan code
   Likelihood<-'
@@ -90,12 +110,14 @@ RRDM<-function(Qmatrix,scale.num,save.path=getwd(),save.name="RRDM"){
   int Y[Np, Ni];
   }
   '
+  
   #Parameter Specification
   parm.spec<-paste(c('parameters{
                      simplex[Nc] Vc;\n ',
                      paste('real',li_0,';\n '),
                      paste('real<lower=0>',li_1,';\n '),
                      paste('real<lower=0>',ls_0_unique,';\n '),
+                     paste('real<lower=0>',ls_1_unique,';\n '),
                      '}\n')
                    ,collapse='')
   
@@ -113,7 +135,6 @@ RRDM<-function(Qmatrix,scale.num,save.path=getwd(),save.name="RRDM"){
   model.spec<-model.spec[!startsWith(str_remove_all(model.spec," "),"~")]
   
   generatedQuantities.spec<-'
-  \n
   generated quantities {
   vector[Ni] contributionsI;
   matrix[Np,Nc] contributionsPC;
@@ -127,7 +148,6 @@ RRDM<-function(Qmatrix,scale.num,save.path=getwd(),save.name="RRDM"){
     }
   }
 }
-  
   '
 if (.Platform$OS.type == "unix") {
   filename = paste(paste(save.path,save.name,sep='/'),'.stan',sep='')
@@ -147,25 +167,5 @@ sink(NULL)
 
 
 Q=matrix(c(rep(c(1,0,0),9),rep(c(0,1,0),9),rep(c(0,0,1),9)), 27, 3, byrow = T) # 27 questions and 3 attributes
-RRDM(Q,5)
-
-
-# n_attr<-dim(Q)[2]
-# PS<-t(expand.grid(replicate(n_attr, 0:1, simplify = FALSE)))  
-# PfbyI<-Q %*% PS  
-
-
-
-
-# After generation needs to:
-# 1. change the priors of main effects to 1.5,2
-# 2. Change int Y[Np, Ni];
-# 3. change generated quantities
-
-
-# Extract class definition
-#n_attr=4
-#temp.table.col<-unique(apply(combn(rep(c(0,1),n_attr),n_attr),2,function(x){paste(x,collapse = "")}))
-#temp.table.col<-temp.table.col[order(temp.table.col)]
-#temp.table.col
+RSDM(Q,5)
 
