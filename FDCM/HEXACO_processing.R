351d6c21b326b198e19ade76af949d333c1d2909
Alexandra <kislenkova.oskemen@gmail.com>
2025-02-26 14:05:50 -0800

    Sample 1000 participants and select 40 items for 4 latent attributes for HEXACO.

--- FDCM/HEXACO_processing.R
+++ FDCM/HEXACO_processing.R
@@ -11,3 +11,14 @@ summary(df_cleaned)
 table(df_cleaned$HSinc1) 
 table(df_cleaned$HFair9)
 table(df_cleaned$OUnco6)
+
+HEXACO <- df_cleaned[sample(nrow(df_cleaned), 1000), ] 
+write.table(HEXACO,'HEXACO.txt')
+
+sinc <- HEXACO[,c(1:10)]
+fair <- HEXACO[,c(11:20)]
+gree <- HEXACO[,c(21:30)]
+mode <- HEXACO[,c(31:40)]
+ 
+H <- HEXACO[,c(1:40)]
+write.table(H,'Honesty-Humility.txt')
