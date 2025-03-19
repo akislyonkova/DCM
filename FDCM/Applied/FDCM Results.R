69dc82b8da8faf54eb7d11d95031a6093db19cc6
Alexandra <kislenkova.oskemen@gmail.com>
2025-03-19 09:59:04 -0700

    Item level analysis for RSDM (FTI).

--- FDCM/Applied/FDCM Results.R
+++ FDCM/Applied/FDCM Results.R
@@ -11,19 +11,26 @@ n_p <- 1000    # number of people
 n_i <- 27      # number of items 
 n_r <- 5       # number of response options
 n_t <- n_r - 1 # number of thresholds
-n_c <- 8       # number of profiles  
+n_d <- 3       # number of dimensions
+n_c <- 2^n_d   # number of profiles  
+n_id <- 9      # number of items per dimension 
 
 # FTI 
 n_i <- 56      # number of items 
 n_r <- 4       # number of response options
 n_t <- n_r - 1 # number of thresholds
-n_c <- 16      # number of profiles  
+n_d <- 4       # number of dimensions
+n_c <- 2^n_d   # number of profiles 
+n_id <- 14     # number of items per dimension 
+ 
 
 # HEXACO (Humility dimension only)
 n_i <- 40      # number of items 
 n_r <- 7       # number of response options
 n_t <- n_r - 1 # number of thresholds
-n_c <- 16      # number of profiles  
+n_d <- 4       # number of dimensions
+n_c <- 2^n_d   # number of profiles
+n_id <- 10     # number of items per dimension 
 
 ######################################################################################################
 #Descriptive stats
@@ -82,8 +89,8 @@ nrdm_table  <- summary(nrdm)$summary
 rsdm_table  <- summary(rsdm)$summary
 
 # FDCM Plots
-# extracted parameters for FDCM 
 
+# extract the  parameters for FDCM 
 n_params <- 3 # 1)intercepts, 2) main effects, and 3) dispersion for each item
 start_idx <- seq(from = n_c + 1, by = n_i, length.out = 3)
 end_idx <- start_idx + n_i - 1
@@ -99,7 +106,7 @@ item_f <- data.frame(
     fdcm_table[
       start_idx[3]:end_idx[3], 1])
 )
-colnames(item_f) <- c("li_I","li_M", "d")
+colnames(item_f) <- c("i_I","i_M", "d")
 
 # For FDCM computing item probabilities
 t <- matrix(NA,n_i,n_r)
@@ -213,9 +220,9 @@ nrdm.t <- rbind(t0,t1)
 t <- matrix(NA,n_i,n_r)
 k <- 1
 for(i in 1:n_i) {
-  t2=exp(item[i,4]*k+item[i,1])
-  t3=exp((item[i,4]+item[i,5])*k+item[i,1]+item[i,2])
-  t4=exp((item[i,4]+item[i,5]+item[i,6])*k+item[i,1]+item[i,2]+item[i,3])
+  t2=exp(item_n[i,4]*k+item_n[i,1])
+  t3=exp((item_n[i,4]+item_n[i,5])*k+item_n[i,1]+item_n[i,2])
+  t4=exp((item_n[i,4]+item_n[i,5]+item_n[i,6])*k+item_n[i,1]+item_n[i,2]+item_n[i,3])
   t6=1+t2+t3+t4
   t[i,]=c(1/t6,t2/t6,t3/t6,t4/t6)
 }
@@ -261,60 +268,80 @@ for (i in 1:n_i){
 # RSDM Plots
 # extracted parameters for RSDM 
 
-items <- data.frame(lapply(list(9:35, 36:62), function(i) rsdm_table[i, 1]))
-steps_i <- data.frame(lapply(list(63:65, 66:68, 69:71, 72:74), function(i) rsdm_table[i, 1]))
-steps_m <- data.frame(lapply(list(75:77, 78:80, 81:83, 84:86), function(i) rsdm_table[i, 1]))
+start_idx <- seq(from = n_c + 1, by = n_i, length.out = 2)
+end_idx <- start_idx + n_i - 1
 
-steps_i <- steps_i[rep(seq_len(nrow(steps_i)), each = 9), ]
-steps_m <- steps_m[rep(seq_len(nrow(steps_m)), each = 9), ]
+item_r <- data.frame(
+  mapply(function(start, end) rsdm_table[start:end, 1], start_idx, end_idx)
+)
 
-colnames(items) <- c("I","M")
-colnames(steps_i) <- c("I_step1", "I_step2", "I_step3", "I_step4")
-colnames(steps_m) <- c("M_step1", "M_step2", "M_step3", "M_step4")
-row_names <- paste0(rep(c("D1_", "D2_", "D3_"), c(8, 9, 10)), "i", seq(1, n_i))
-rownames(steps_i) <- row_names
-rownames(steps_m) <- row_names
+start_idx <- seq(from = n_c + 2*n_i + 1, by = n_d, length.out = 2*n_t)
+end_idx <- start_idx + n_d - 1
+
+step_r <- data.frame(
+  mapply(function(start, end) rsdm_table[start:end, 1], start_idx, end_idx)
+)
+step_r <- step_r[rep(seq_len(nrow(step_r)), each = n_id), ]
+
+colnames(item_r) <- c("i_I","i_M")
+rownames(item_r) <- paste0("item_", seq(1, n_i))
+
+colnames(step_r) <- paste0(rep(c("i", "m"), each = n_t), "_step", rep(1:n_t, 2))
+rownames(step_r) <- paste("item", rep(1:n_i, 1), "_D", rep(1:n_d, each = n_id), sep="")
 
 
 t <- matrix(NA,n_i,n_r)  
-k <- 1 
-
+#For Dark Triad
 for(i in 1:n_i) {
-  t2 <- exp((items[i,2]+steps_m[i,1])*k+items[i,1]-steps_i[i,1])
-  t3 <- exp((items[i,2]+steps_m[i,1]+steps_m[i,2])*k+items[i,1]-steps_i[i,1]-steps_i[i,2])
-  t4 <- exp((items[i,2]+steps_m[i,1]+steps_m[i,2]+steps_m[i,3])*k+items[i,1]-steps_i[i,1]-steps_i[i,2]-steps_i[i,3])
-  t5 <- exp((items[i,2]+steps_m[i,1]+steps_m[i,2]+steps_m[i,3]+steps_m[i,4])*k+items[i,1]-steps_i[i,1]-steps_i[i,2]-steps_i[i,3]-steps_i[i,4])
+  t2 <- exp((item_r[i,2]+steps_m[i,1])*k+item_r[i,1]-steps_i[i,1])
+  t3 <- exp((item_r[i,2]+steps_m[i,1]+steps_m[i,2])*k+item_r[i,1]-steps_i[i,1]-steps_i[i,2])
+  t4 <- exp((item_r[i,2]+steps_m[i,1]+steps_m[i,2]+steps_m[i,3])*k+item_r[i,1]-steps_i[i,1]-steps_i[i,2]-steps_i[i,3])
+  t5 <- exp((item_r[i,2]+steps_m[i,1]+steps_m[i,2]+steps_m[i,3]+steps_m[i,4])*k+item_r[i,1]-steps_i[i,1]-steps_i[i,2]-steps_i[i,3]-steps_i[i,4])
   t6 <- 1+t2+t3+t4+t5
   t[i,] <- c(1/t6,t2/t6,t3/t6,t4/t6,t5/t6)
 }
 
+#For FTI
+for(i in 1:n_i) {
+  t2 <- exp((item_r[i,2]+step_r[i,4])*k+item_r[i,1]-step_r[i,1])
+  t3 <- exp((item_r[i,2]+step_r[i,4]+step_r[i,5])*k+item_r[i,1]-step_r[i,1]-step_r[i,2])
+  t4 <- exp((item_r[i,2]+step_r[i,4]+step_r[i,5]+step_r[i,6])*k+item_r[i,1]-step_r[i,1]-step_r[i,2]-step_r[i,3])
+  t6 <- 1+t2+t3+t4
+  t[i,] <- c(1/t6,t2/t6,t3/t6,t4/t6)
+}
+
+k <- 1 
 t1 <- t 
 t <- matrix(NA,n_i,n_r) 
 k <- 0 
-for(i in 1:n_i) {
-  t2 <- exp((items[i,2]+steps_m[i,1])*k+items[i,1]-steps_i[i,1])
-  t3 <- exp((items[i,2]+steps_m[i,1]+steps_m[i,2])*k+items[i,1]-steps_i[i,1]-steps_i[i,2])
-  t4 <- exp((items[i,2]+steps_m[i,1]+steps_m[i,2]+steps_m[i,3])*k+items[i,1]-steps_i[i,1]-steps_i[i,2]-steps_i[i,3])
-  t5 <- exp((items[i,2]+steps_m[i,1]+steps_m[i,2]+steps_m[i,3]+steps_m[i,4])*k+items[i,1]-steps_i[i,1]-steps_i[i,2]-steps_i[i,3]-steps_i[i,4])
-  t6 <- 1+t2+t3+t4+t5
-  t[i,] <- c(1/t6,t2/t6,t3/t6,t4/t6,t5/t6)
-}
 t0 <- t 
 rsdm.t <- rbind(t0,t1) 
+
+
+
+
+
 
 # RSDM Plots
 rsdm.t <- round(rsdm.t,4)
 gr <- c(rep("no attribute",n_i),rep("attribute",n_i),rep("o0",n_i),rep("o1",n_i))
 id <- c(rep(c(1:n_i),4))
 rsdm.t <- data.frame(cbind(id,gr,rsdm.t))
+
+#For Dark Triad
 colnames(rsdm.t) <- c("item","class", "Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree")
 rsdm.t[,3:7] <- sapply(rsdm.t[,3:7],as.numeric) 
-summary(rsdm.t)
-
 dfm <- melt(rsdm.t, id.vars=c("item", "class"),
             measure.vars = c("Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"))
 
-path = file.path(getwd(), 'plots/')
+#For FTI
+colnames(rsdm.t) <- c("item","class", "Strongly Disagree", "Disagree", "Agree", "Strongly Agree")
+rsdm.t[,3:6] <- sapply(rsdm.t[,3:6],as.numeric) 
+dfm <- melt(rsdm.t, id.vars=c("item", "class"),
+            measure.vars = c("Strongly Disagree", "Disagree", "Agree", "Strongly Agree"))
+
+
+path <- file.path(getwd(), 'RSDM plots/')
 dir.create(path)
 # save the plots for every item
 for (i in 1:n_i){
