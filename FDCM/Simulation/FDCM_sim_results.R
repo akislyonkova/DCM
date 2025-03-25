74b510ccff0a2f054a1148fa2270d84eb1956312
Alexandra <kislenkova.oskemen@gmail.com>
2025-03-25 15:12:12 -0700

    Updated the code for the FDCM simulation part: made it simpler and more flexible, plus added the RMSE part.

--- FDCM/Simulation/FDCM_sim_results.R
+++ FDCM/Simulation/FDCM_sim_results.R
@@ -1,27 +1,31 @@
-### Cell 1
 
-n_sim <- 3  
+cell <- 2
+n_sim <- 20 
 rows_start <- 9 
 rows_end <- 89 
 
-fdcm_param <- read.table("cell1_param.txt") 
+file_name <- paste("cell", cell, "_param.txt", sep="")
+fdcm_param <- read.table(file_name)
+rm(fdcm_param)
 
 
-sim_param <- NA 
+sim_param <- NULL
 for (i in 1:n_sim) { 
-  sim_name <- sprintf("FDCM_sim%i2025-02-17.txt", i)
+  sim_name <- sprintf("FDCM_sim%i.txt", i)
   sim_param <- cbind(sim_param,read.table(sim_name)[c(rows_start:rows_end),1])
 }
-sim_param <- sim_param[,-1]  
-
-
-### Calculating raw bias
-fdcm_param_n <- data.frame(rep(fdcm_param, n_sim)) # duplicating the columns with the original parameters
-sim_param_dif <- (sim_param - fdcm_param_n) # calulating the differences 
+ 
+fdcm_param_n <- data.frame(rep(fdcm_param, n_sim)) 
+sim_param_dif <- (sim_param - fdcm_param_n)  
 for (i in 1:n_sim) {
-  colnames(sim_param_dif)[i] <- paste("sim", i, sep='') # renaming the columns 
+  colnames(sim_param_dif)[i] <- paste("sim", i, sep='') 
 }
 
-bias <- data.frame(rowSums(sim_param_dif)/n_sim) # calculating raw bias 
-colnames(bias) <- "raw_bias" # renaming the column
+bias <- data.frame(rowSums(sim_param_dif)/n_sim)  
+colnames(bias) <- "raw_bias" 
 
+hist(bias$raw_bias)
+
+bias$rmse <- NA 
+bias$rmse <- sqrt((rowSums(sim_param_dif))^2/(n_sim-1)) 
+hist(bias$rmse) 
